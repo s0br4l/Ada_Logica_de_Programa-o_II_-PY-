db585d8866f96c1808a2c49105ccbbd502022e34
lsobral <lucas.sobral5@gmail.com>
2023-07-28 22:34:54 -0300

    add clear func

--- logica_de_programacao_II/projetos/forca/forca.py
+++ logica_de_programacao_II/projetos/forca/forca.py
@@ -1,5 +1,6 @@
 from erros_forca import desenha_forca
 from funcoes import *
+import os
 
 
 def jogo_da_forca():
@@ -31,22 +32,26 @@ def jogo_da_forca():
             # Pede ao jogador para digitar uma letra
             try:
                 letra_digitada = input("Digite uma letra: ").lower()
+                os.system('cls' if os.name == 'nt' else 'clear')
             except:
                 print("O usuário interrompeu o programa.")
                 return
 
             # Verifica se a letra já foi digitada antes
             if letra_digitada in letras_digitadas:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 print("Você já digitou essa letra.")
                 print(f"As letras digitadas foram: ", letras_digitadas)
 
             # Adiciona a letra à lista de letras acertadas, caso ela esteja na palavra
             elif letra_digitada in fruta_selecionada:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 letras_acertadas.append(letra_digitada)
                 letras_digitadas.append(letra_digitada)
                 print(f"As letras digitadas foram: ", letras_digitadas)
 
             else:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 print("A letra não faz parte da palavra.")
                 num_erros += 1
                 desenha_forca(num_erros)
